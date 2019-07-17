ccf27bd823db0592ddae7ae68784906b3c258494
Aaron Wright <aaron.c.wright@icloud.com>
2019-07-17 16:07:04 -0500

    Add missing header keys

--- Sources/Pinball.swift
+++ Sources/Pinball.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Combine
 
 public struct Pinball {
     
@@ -45,14 +46,152 @@ public struct Pinball {
     
     public enum Header: Equatable {
         case accept(String)
+        case acceptCharset(String)
+        case acceptEncoding(String)
+        case acceptLanguage(String)
+        case acceptRanges(String)
+        case age(String)
+        case allow(String)
+        case authorization(String)
+        case cacheControl(String)
+        case connection(String)
+        case contentEncoding(String)
+        case contentLanguage(String)
+        case contentLength(String)
+        case contentLocation(String)
+        case contentMD5(String)
+        case contentRange(String)
         case contentType(String)
+        case custom(String, String)
+        case date(String)
+        case etag(String)
+        case expect(String)
+        case expires(String)
+        case from(String)
+        case host(String)
+        case ifMatch(String)
+        case ifModifiedSince(String)
+        case ifNoneMatch(String)
+        case ifRange(String)
+        case ifUnmodifiedSince(String)
+        case lastModified(String)
+        case location(String)
+        case maxForwards(String)
+        case pragma(String)
+        case proxyAuthenticate(String)
+        case proxyAuthorization(String)
+        case range(String)
+        case referer(String)
+        case retryAfter(String)
+        case server(String)
+        case te(String)
+        case trailer(String)
+        case transferEncoding(String)
+        case upgrade(String)
+        case userAgent(String)
+        case vary(String)
+        case via(String)
+        case warning(String)
+        case wwwAuthenticate(String)
         
         var data: (key: String, value: String) {
             switch self {
             case .accept(let value):
                 return (key: "Accept", value: value)
+            case .acceptCharset(let value):
+                return (key: "Accept-Charset", value: value)
+            case .acceptEncoding(let value):
+                return (key: "Accept-Encoding", value: value)
+            case .acceptLanguage(let value):
+                return (key: "Accept-Language", value: value)
+            case .acceptRanges(let value):
+                return (key: "Accept-Ranges", value: value)
+            case .age(let value):
+                return (key: "Age", value: value)
+            case .allow(let value):
+                return (key: "Allow", value: value)
+            case .authorization(let value):
+                return (key: "Authorization", value: value)
+            case .cacheControl(let value):
+                return (key: "Cache-Control", value: value)
+            case .connection(let value):
+                return (key: "Connection", value: value)
+            case .contentEncoding(let value):
+                return (key: "Content-Encoding", value: value)
+            case .contentLanguage(let value):
+                return (key: "Content-Language", value: value)
+            case .contentLength(let value):
+                return (key: "Content-Length", value: value)
+            case .contentLocation(let value):
+                return (key: "Content-Location", value: value)
+            case .contentMD5(let value):
+                return (key: "Content-MD5", value: value)
+            case .contentRange(let value):
+                return (key: "Content-Range", value: value)
             case .contentType(let value):
                 return (key: "Content-Type", value: value)
+            case .custom(let key, let value):
+                return (key: key, value: value)
+            case .date(let value):
+                return (key: "Date", value: value)
+            case .etag(let value):
+                return (key: "ETag", value: value)
+            case .expect(let value):
+                return (key: "Expect", value: value)
+            case .expires(let value):
+                return (key: "Expires", value: value)
+            case .from(let value):
+                return (key: "From", value: value)
+            case .host(let value):
+                return (key: "Host", value: value)
+            case .ifMatch(let value):
+                return (key: "If-Match", value: value)
+            case .ifModifiedSince(let value):
+                return (key: "If-Modified-Since", value: value)
+            case .ifNoneMatch(let value):
+                return (key: "If-None-Match", value: value)
+            case .ifRange(let value):
+                return (key: "If-Range", value: value)
+            case .ifUnmodifiedSince(let value):
+                return (key: "If-Unmodified-Since", value: value)
+            case .lastModified(let value):
+                return (key: "Last-Modified", value: value)
+            case .location(let value):
+                return (key: "Location", value: value)
+            case .maxForwards(let value):
+                return (key: "Max-Forwards", value: value)
+            case .pragma(let value):
+                return (key: "Pragma", value: value)
+            case .proxyAuthenticate(let value):
+                return (key: "Proxy-Authenticate", value: value)
+            case .proxyAuthorization(let value):
+                return (key: "Proxy-Authorization", value: value)
+            case .range(let value):
+                return (key: "Range", value: value)
+            case .referer(let value):
+                return (key: "Referer", value: value)
+            case .retryAfter(let value):
+                return (key: "Retry-After", value: value)
+            case .server(let value):
+                return (key: "Server", value: value)
+            case .te(let value):
+                return (key: "TE", value: value)
+            case .trailer(let value):
+                return (key: "Trailer", value: value)
+            case .transferEncoding(let value):
+                return (key: "Transfer-Encoding", value: value)
+            case .upgrade(let value):
+                return (key: "Upgrade", value: value)
+            case .userAgent(let value):
+                return (key: "User-Agent", value: value)
+            case .vary(let value):
+                return (key: "Vary", value: value)
+            case .via(let value):
+                return (key: "Via", value: value)
+            case .warning(let value):
+                return (key: "Warning", value: value)
+            case .wwwAuthenticate(let value):
+                return (key: "WWW-Authenticate", value: value)
             }
         }
     }
@@ -134,7 +273,7 @@ public extension URLSession {
     
 }
 
-@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension URLSession {
     
     func dataTaskPublisher(for endpoint: Pinball.Endpoint) throws -> URLSession.DataTaskPublisher {
--- Tests/PinballTests.swift
+++ Tests/PinballTests.swift
@@ -78,8 +78,146 @@ final class PinballTests: XCTestCase {
             case .accept(let value):
                 XCTAssertEqual(data.key, "Accept")
                 XCTAssertEqual(data.value, value)
+            case .acceptCharset(let value):
+                XCTAssertEqual(data.key, "Accept-Charset")
+                XCTAssertEqual(data.value, value)
+            case .acceptEncoding(let value):
+                XCTAssertEqual(data.key, "Accept-Encoding")
+                XCTAssertEqual(data.value, value)
+            case .acceptLanguage(let value):
+                 XCTAssertEqual(data.key, "Accept-Language")
+                XCTAssertEqual(data.value, value)
+            case .acceptRanges(let value):
+                XCTAssertEqual(data.key, "Accept-Ranges")
+                XCTAssertEqual(data.value, value)
+            case .age(let value):
+                XCTAssertEqual(data.key, "Age")
+                XCTAssertEqual(data.value, value)
+            case .allow(let value):
+                XCTAssertEqual(data.key, "Allow")
+                XCTAssertEqual(data.value, value)
+            case .authorization(let value):
+                 XCTAssertEqual(data.key, "Authorization")
+                XCTAssertEqual(data.value, value)
+            case .cacheControl(let value):
+                XCTAssertEqual(data.key, "Cache-Control")
+                XCTAssertEqual(data.value, value)
+            case .connection(let value):
+                XCTAssertEqual(data.key, "Connection")
+                XCTAssertEqual(data.value, value)
+            case .contentEncoding(let value):
+                XCTAssertEqual(data.key, "Content-Encoding")
+                XCTAssertEqual(data.value, value)
+            case .contentLanguage(let value):
+                XCTAssertEqual(data.key, "Content-Language")
+                XCTAssertEqual(data.value, value)
+            case .contentLength(let value):
+                XCTAssertEqual(data.key, "Content-Length")
+                XCTAssertEqual(data.value, value)
+            case .contentLocation(let value):
+                XCTAssertEqual(data.key, "Content-Location")
+                XCTAssertEqual(data.value, value)
+            case .contentMD5(let value):
+                XCTAssertEqual(data.key, "Content-MD5")
+                XCTAssertEqual(data.value, value)
+            case .contentRange(let value):
+                XCTAssertEqual(data.key, "Content-Range")
+                XCTAssertEqual(data.value, value)
             case .contentType(let value):
                 XCTAssertEqual(data.key, "Content-Type")
+                XCTAssertEqual(data.value, value)
+            case .custom(let key, let value):
+                XCTAssertEqual(data.key, key)
+                XCTAssertEqual(data.value, value)
+            case .date(let value):
+                XCTAssertEqual(data.key, "Date")
+                XCTAssertEqual(data.value, value)
+            case .etag(let value):
+                XCTAssertEqual(data.key, "ETag")
+                XCTAssertEqual(data.value, value)
+            case .expect(let value):
+                XCTAssertEqual(data.key, "Except")
+                XCTAssertEqual(data.value, value)
+            case .expires(let value):
+                XCTAssertEqual(data.key, "Expires")
+                XCTAssertEqual(data.value, value)
+            case .from(let value):
+                XCTAssertEqual(data.key, "From")
+                XCTAssertEqual(data.value, value)
+            case .host(let value):
+                XCTAssertEqual(data.key, "Host")
+                XCTAssertEqual(data.value, value)
+            case .ifMatch(let value):
+                XCTAssertEqual(data.key, "If-Match")
+                XCTAssertEqual(data.value, value)
+            case .ifModifiedSince(let value):
+                XCTAssertEqual(data.key, "If-Modified-Since")
+                XCTAssertEqual(data.value, value)
+            case .ifNoneMatch(let value):
+                XCTAssertEqual(data.key, "If-None-Match")
+                XCTAssertEqual(data.value, value)
+            case .ifRange(let value):
+                XCTAssertEqual(data.key, "If-Range")
+                XCTAssertEqual(data.value, value)
+            case .ifUnmodifiedSince(let value):
+                XCTAssertEqual(data.key, "If-Unmodified-Since")
+                XCTAssertEqual(data.value, value)
+            case .lastModified(let value):
+                XCTAssertEqual(data.key, "Last-Modified")
+                XCTAssertEqual(data.value, value)
+            case .location(let value):
+                XCTAssertEqual(data.key, "Location")
+                XCTAssertEqual(data.value, value)
+            case .maxForwards(let value):
+                XCTAssertEqual(data.key, "Max-Forwards")
+                XCTAssertEqual(data.value, value)
+            case .pragma(let value):
+                XCTAssertEqual(data.key, "Pragma")
+                XCTAssertEqual(data.value, value)
+            case .proxyAuthenticate(let value):
+                XCTAssertEqual(data.key, "Proxy-Authenticate")
+                XCTAssertEqual(data.value, value)
+            case .proxyAuthorization(let value):
+                XCTAssertEqual(data.key, "Proxy-Authorization")
+                XCTAssertEqual(data.value, value)
+            case .range(let value):
+                XCTAssertEqual(data.key, "Range")
+                XCTAssertEqual(data.value, value)
+            case .referer(let value):
+                XCTAssertEqual(data.key, "Referer")
+                XCTAssertEqual(data.value, value)
+            case .retryAfter(let value):
+                XCTAssertEqual(data.key, "Retry-After")
+                XCTAssertEqual(data.value, value)
+            case .server(let value):
+                XCTAssertEqual(data.key, "Server")
+                XCTAssertEqual(data.value, value)
+            case .te(let value):
+                XCTAssertEqual(data.key, "TE")
+                XCTAssertEqual(data.value, value)
+            case .trailer(let value):
+                XCTAssertEqual(data.key, "Trailer")
+                XCTAssertEqual(data.value, value)
+            case .transferEncoding(let value):
+                XCTAssertEqual(data.key, "Transfer-Encoding")
+                XCTAssertEqual(data.value, value)
+            case .upgrade(let value):
+                XCTAssertEqual(data.key, "Upgrade")
+                XCTAssertEqual(data.value, value)
+            case .userAgent(let value):
+                XCTAssertEqual(data.key, "User-Agent")
+                XCTAssertEqual(data.value, value)
+            case .vary(let value):
+                XCTAssertEqual(data.key, "Vary")
+                XCTAssertEqual(data.value, value)
+            case .via(let value):
+                XCTAssertEqual(data.key, "Via")
+                XCTAssertEqual(data.value, value)
+            case .warning(let value):
+                XCTAssertEqual(data.key, "Warning")
+                XCTAssertEqual(data.value, value)
+            case .wwwAuthenticate(let value):
+                XCTAssertEqual(data.key, "WWW-Authenticate")
                 XCTAssertEqual(data.value, value)
             }
         }
@@ -101,15 +239,19 @@ final class PinballTests: XCTestCase {
         }
     }
     
-    @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension PinballTests {
+    
     func testCanBuildURLSessionPublisher() {
         let endpoint = Pinball.Endpoint(method: .get, scheme: .https, host: "localhost", port: 3000, user: "example", password: "password", paths: paths, queries: queries, headers: headers, data: nil)
-        
+
         do {
             let _ = try URLSession.shared.dataTaskPublisher(for: endpoint)
         } catch {
             XCTFail(error.localizedDescription)
         }
     }
-    
+
 }
