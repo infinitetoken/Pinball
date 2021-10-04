4c60c7189e0117501c110ebd996f751459b53099
Aaron Wright <aaron.c.wright@icloud.com>
2021-10-03 23:06:50 -0500

    Fix iOS build error

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     name: "Pinball",
     platforms: [
         .macOS(.v10_12),
-        .iOS(.v10),
+        .iOS(.v12),
         .tvOS(.v10),
         .watchOS(.v3)
     ],
